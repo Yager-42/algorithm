b4c9bbf9a9dfb05b899d254647b8301113f52f99
Yager-42 <82296072+Yager-42@users.noreply.github.com>
2021-07-19 16:41:25 +0800

    2021.7.19
    今天新增了循环链表并且解决了约瑟夫问题

--- linearlist.h
+++ linearlist.h
@@ -3,6 +3,7 @@
 #define LINEAR_LIST_H
 #define MAXSIZE 1000
 #include<iostream>
+#include<time.h>
 using namespace std;
 
 template<typename T>
@@ -76,7 +77,52 @@ private:
 public:
 	chain()
 	{
-		initListTail();
+		josephus();
+	}
+	void initRlist()
+	{
+		head->next = head;
+		Node<T>* temp;
+		Node<T>* endP_before;
+		cout << "the length pls " << endl;
+		cin >> length;
+		for (int i = 0; i < length; ++i)
+		{
+			for (endP_before = head->next; endP_before->next != head; endP_before = endP_before->next);
+			cout << "enter the data of " << i + 1 << " element" << endl;
+			temp = new Node<T>;
+			cin >> temp->data;
+			temp->next = head;
+			endP_before->next = temp;
+			
+		}
+	}
+	void random_initList()
+	{
+		head->next = NULL;
+		srand(time(0));
+		length = 20;
+		for (int i = 0; i < length; ++i)
+		{
+			Node<T>* temp = new Node<T>;
+			temp->data = (T)rand();
+			temp->next = head->next;
+			head->next = temp;
+		}
+	}
+	void random_initList_Tail()
+	{
+		Node<T>* temp = head;
+		head->next = NULL;
+		srand(time(0));
+		length = 20;
+		for (int i = 0; i < length; ++i)
+		{
+			Node<T>* p = new Node<T>;
+			p->data = (T)rand();
+			temp->next = p;
+			temp = p;
+		}
 	}
 	void initList()
 	{
@@ -107,6 +153,17 @@ public:
 			temp = p;
 		}
 		temp->next = NULL;
+	}
+	T getMidNode()
+	{
+		Node<T>* fast = head;
+		Node<T>* low = head;
+		while (fast->next!=NULL && fast!=NULL)
+		{
+			fast =fast->next->next;
+			low=low->next;
+		}
+		return low->data;
 	}
 	bool getElem(int index,Node<T>* element)
 	{
@@ -195,12 +252,112 @@ public:
 	void show()
 	{
 		Node<T>* temp;
+		int i = 0;
 		temp = head->next;
-		for (int i = 0; i < length; ++i)
+		while(temp!=NULL&&temp!=head)
 		{
 			cout <<"第"<<i+1<<"个元素： "<< temp->data << endl;
 			temp = temp->next;
+			++i;
 		}
+	}
+	void Rinsert(int index)
+	{
+		Node<T>* temp=new Node<T>;
+		Node<T>* target=head->next;
+		Node<T>* p;
+		cout << "the value you want to insert: " << endl;
+		cin >> temp->data;
+		if (index == 0)
+		{
+			head->next = temp;
+			temp->next = head;
+		}
+		else
+		{
+			for (int i = 0; i < index - 1; ++i)
+			{
+				target = target->next;
+			}
+			p = target->next;
+			target->next = temp;
+			temp->next = p;
+		}
+
+	}
+	void Rdelete(int index)
+	{
+		Node<T>* temp;
+		Node<T>* target;
+		if (index == 0)
+		{
+			temp = head->next;
+			head->next = temp->next;
+			delete temp;
+			temp = NULL;
+		}
+		else
+		{
+			target = head->next;
+			for (int i = 0; i < index - 1; ++i)
+			{
+				target = target->next;
+			}
+			temp = target->next;
+			target->next = temp->next;
+			delete temp;
+			temp = NULL;
+		}
+	}
+	int Rsearch(T data)
+	{
+		Node<T>* temp = head->next;
+		int i = 0;
+		while (temp != head)
+		{
+			if (temp->data == data)
+			{
+				return i;
+			}
+			++i;
+			temp = temp->next;
+		}
+		return -1;
+	}
+	/// <summary>
+	/// 上面的代码由于一开始的理解有偏差，头节点和头指针指向的地方不一样，我把头指针的数据防空，没东西，下面开始就是一样的了
+	/// </summary>
+	void josephus()
+	{
+		head->next = head;
+		head->data = 1;
+		length = 41;
+		Node<T>* temp;
+		Node<T>* target;
+		for (int i = 1; i < length; ++i)
+		{
+			temp = new Node<T>;
+			temp->data = i + 1;
+			for (target = head; target->next != head; target = target->next);
+			temp->next = head;
+			target->next = temp;
+		}//生成1到41的循环链表
+		Node<T>* handle = head;
+		Node<T>* tempH = NULL;
+		int times = 0;
+		while (handle->next!=handle)
+		{
+			handle = handle->next;
+			cout << handle->next->data << " has suicided" ;
+			tempH = handle->next;
+			handle->next = handle->next->next;
+			handle = handle->next;
+			delete tempH;
+			tempH = NULL;
+			++times;
+			cout << "  " << times << endl;
+		}
+
 	}
 
 
